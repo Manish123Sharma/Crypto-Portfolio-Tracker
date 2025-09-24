e1abf69dada1fc37313f8d3fae4ff4f627e4930a
Manish <manish2003sharma@gmail.com>
2025-09-24 16:30:14 +0530

    models

--- lib/models/coin.dart
+++ lib/models/coin.dart
@@ -1,49 +1,35 @@
-import 'package:hive/hive.dart';
-
-part 'coin.g.dart';
-
-@HiveType(typeId: 0)
-class Coin extends HiveObject {
-    @HiveField(0)
+class Coin {
     final String id;
-
-    @HiveField(1)
-    final String name;
-
-    @HiveField(2)
     final String symbol;
-
-    @HiveField(3)
+    final String name;
+    final String image;
     final double price;
-
-    @HiveField(4)
-    final String logoUrl;
 
     Coin({
         required this.id,
-        required this.name,
         required this.symbol,
-        required this.price,
-        required this.logoUrl,
+        required this.name,
+        required this.image,
+        required this.price
     });
 
-    factory Coin.fromJson(Map<String, dynamic> json, {String? logo}) {
+    factory Coin.fromJson(Map<String, dynamic> json) {
         return Coin(
             id: json['id'],
-            name: json['name'],
+            price: (json['current_price'] ?? 0).toDouble(),
             symbol: json['symbol'],
-            price: (json['current_price'] as num).toDouble(),
-            logoUrl: logo ?? '',
+            name: json['name'],
+            image: json['image'] ?? '', // fallback empty string
         );
     }
 
     Map<String, dynamic> toJson() {
         return {
             'id': id,
-            'name': name,
             'symbol': symbol,
+            'name': name,
+            'image': image,
             'price': price,
-            'logoUrl': logoUrl,
         };
     }
 }
