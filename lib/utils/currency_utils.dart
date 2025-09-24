b7f4c78929235456b5dde97715963e496f9e4e12
Manish <manish2003sharma@gmail.com>
2025-09-24 16:25:54 +0530

    Removals

--- lib/utils/currency_utils.dart
+++
@@ -1,6 +0,0 @@
-import 'package:intl/intl.dart';
-
-String formatCurrency(double value) {
-  final f = NumberFormat.currency(locale: 'en_US', symbol: '\$');
-  return f.format(value);
-}
--- lib/utils/formatters.dart
+++
@@ -1,7 +0,0 @@
-import 'package:intl/intl.dart';
-
-final _currencyFormatter = NumberFormat.simpleCurrency(decimalDigits: 2);
-
-String formatCurrency(double value) {
-  return _currencyFormatter.format(value);
-}
